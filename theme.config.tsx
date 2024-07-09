ab0041208bd89ec8dec26feaa4f1acffd9197752
Xjph <archseraphim@gmail.com>
2024-07-08 22:34:39 -0230

    chore: migration from DokuWiki

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Elite Observatory</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Xjph/ObservatoryCore',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/RAFDHsY',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Xjph/observatory-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '',
   },
 }
 
